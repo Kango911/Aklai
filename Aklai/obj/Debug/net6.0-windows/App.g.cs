03c2199932ce3327af42964645f659b3ba7f291f
Anatoliy <magic-t@bk.ru>
2023-03-04 12:38:27 +0500

    Redid xaml without coordinates

--- Aklai/obj/Debug/net6.0-windows/App.g.cs
+++ Aklai/obj/Debug/net6.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace Aklai {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace Aklai {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public static void Main() {
             Aklai.App app = new Aklai.App();
             app.InitializeComponent();
